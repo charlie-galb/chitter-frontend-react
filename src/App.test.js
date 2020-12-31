01bce0fe49308e8905eeb213947c82be7604a744
charlie-galb <galbraith.charlie@gmail.com>
2020-12-31 13:32:40 +0000

    Modify default test to reflect deletions

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders without crashing', () => {
+  const root = document.createElement("div");
+  ReactDOM.render(<App />, root);
+  expect(root.querySelector("h1").textContent).toBe("This is the app div");
 });
