d21f8eb40d16ea3657b7031783fc4da1a6704cb3
charlie-galb <galbraith.charlie@gmail.com>
2021-01-02 12:44:18 +0000

    Add test for API request

--- src/components/PeepList/PeepList.test.js
+++ src/components/PeepList/PeepList.test.js
@@ -1,9 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from "axios";
 import PeepList from './PeepList.js';
 
-test('renders without crashing', () => {
-  const peepListContainer = document.createElement("div");
-  ReactDOM.render(<PeepList />, peepListContainer);
-  expect(peepListContainer.querySelector("p").textContent).toBe("OneTwoThree");
-});
+ describe("PeepList", () => {
+
+  beforeAll(() => {
+    jest.mock("axios")
+  })
+
+  afterAll(() => {
+    jest.resetAllMocks()
+  })
+
+  test('renders without crashing', () => {
+    const peepListContainer = document.createElement("div");
+    ReactDOM.render(<PeepList />, peepListContainer);
+    expect(peepListContainer.querySelector("p").textContent).toBe("OneTwoThree");
+  });
+
+  test('makes a get request', async () => {
+    let axiosSpy = jest.spyOn(axios, "get")
+    const peepListContainer = document.createElement("div");
+    await ReactDOM.render(<PeepList />, peepListContainer)
+    expect(axiosSpy).toHaveBeenCalledWith("https://chitter-backend-api-v2.herokuapp.com/peeps")
+  });
+
+ }) 
