8f5b9ac9f1b23f81c853ec98c877de90d2417bbd
charlie-galb <galbraith.charlie@gmail.com>
2021-01-02 12:41:38 +0000

    Add eslintcache

--- src/components/PeepList/PeepList.js
+++ src/components/PeepList/PeepList.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import Axios from 'axios';
 
 const PeepList = () => {
 
     let list = ["One", "Two", "Three"]
 
+    let peeps;
+
+    useEffect() {
+        try {
+            const response = await Axios.get("https://chitter-backend-api-v2.herokuapp.com/peeps");
+            if (response){
+                console.log(response)
+                peeps = response
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
     return (
         <div>
             <p>{list}</p>
+        
         </div>
     )
 }
