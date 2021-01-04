66a7a0ea83c8550ff1e59843d67beb3327af274d
charlie-galb <galbraith.charlie@gmail.com>
2021-01-04 16:26:56 +0000

    Add navbar component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import PeepList from './components/PeepList/PeepList.js'
+import Navbar from './components/Navbar/Navbar.js'
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <h1>This is the app div</h1>
       <PeepList />
     </div>
