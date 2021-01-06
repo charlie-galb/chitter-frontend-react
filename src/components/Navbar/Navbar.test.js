4dd142c895e8e5cbb6807d7a0eb18a640c79feca
charlie-galb <galbraith.charlie@gmail.com>
2021-01-06 11:03:51 +0000

    Remove unused imports

--- src/components/Navbar/Navbar.test.js
+++ src/components/Navbar/Navbar.test.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { render} from "@testing-library/react"
-import { getByTestId } from "@testing-library/dom"
+import { render} from "@testing-library/react";
+import {BrowserRouter as Router} from 'react-router-dom';
 import Navbar from './Navbar.js';
 
 describe("PeepList", () => {
   
   test('renders without crashing', () => {
-    const navbar = render(<Navbar />);
+    const navbar = render(<Router><Navbar /></Router>);
     expect(navbar.getByTestId("sign-up-link").textContent).toBe("Sign up");
   });
 
