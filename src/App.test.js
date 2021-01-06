f25b3fd9e3791d80ca3fe2450b1c5b8bfb3a310d
charlie-galb <galbraith.charlie@gmail.com>
2021-01-06 11:03:03 +0000

    Modify test after removalof p element in app

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from "@testing-library/react";
 import App from './App';
 
 test('renders without crashing', () => {
-  const root = document.createElement("div");
-  ReactDOM.render(<App />, root);
-  expect(root.querySelector("h1").textContent).toBe("This is the app div");
+    const app = render(<App />);
+    expect(app.getByTestId("sign-up-link").textContent).toBe("Sign up");
 });
