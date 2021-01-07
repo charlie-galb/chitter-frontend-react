22c969bdf14286e20794d9d06d0320ef085bdeaa
charlie-galb <galbraith.charlie@gmail.com>
2021-01-07 14:40:02 +0000

    Remove log in and sign up links

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -12,12 +12,6 @@ function Navbar() {
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav">
             <li className="nav-item">
-              <a className="nav-link" id="nav-sign-up-link" data-testid="nav-sign-up-link" href="#">Sign up</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" id="nav-log-in-link" data-testid="nav-log-in-link" href="#">Log in</a>
-            </li>
-            <li className="nav-item">
               <Link to='/' className="nav-link" id="log-out-link" data-testid="log-out-link" >Log out</Link>
             </li>
           </ul>
