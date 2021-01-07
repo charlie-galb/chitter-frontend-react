67d8bc98491ca1d5033e2497ba012ff0e77a593a
charlie-galb <galbraith.charlie@gmail.com>
2021-01-07 15:32:06 +0000

    Add SignupForm route

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-import Timeline from './components/Timeline/Timeline.js'
-import Home from './components/Home/Home.js'
-import Navbar from './components/Navbar/Navbar.js'
+import Timeline from './components/Timeline/Timeline.js';
+import Home from './components/Home/Home.js';
+import Navbar from './components/Navbar/Navbar.js';
+import SignupForm from './components/SignupForm/SignupForm.js';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Switch>
           <Route exact path='/' component={Home} />
           <Route path='/timeline' component={Timeline} />
+          <Route path='/sign_up' component={SignupForm} />
         </Switch>
       </div>
     </Router>
