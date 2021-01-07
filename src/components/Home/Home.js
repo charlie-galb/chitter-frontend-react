840d22d038b6e393348dc8f9264b747463c8eb49
charlie-galb <galbraith.charlie@gmail.com>
2021-01-07 14:31:28 +0000

    Add simple headers, LoginForm child, and Link to sign-up (not connected)"

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,16 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
+import LoginForm from '../LoginForm/LoginForm.js';
 
 export default class Home extends Component {
     render() {
         return (
             <div>
-                <p>Welcome to Chitter</p>
+                <h1 data-testid="home-h1">Welcome to Chitter!</h1>
+                <h2 data-testid="home-h2">Please log in below</h2>
+                <LoginForm />
+                <p>Don't have an account?</p>
+                <Link data-testid="sign-up-link" to='/sign_up'>Sign up here</Link>
             </div>
         )
     }
