429fb0856b42c7702752fe40af66a83c5c9119f5
charlie-galb <galbraith.charlie@gmail.com>
2021-01-07 14:36:59 +0000

    Add container for PeepList

---
+++ src/components/Timeline/Timeline.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import PeepList from '../PeepList/PeepList.js';
+
+export default class Timeline extends Component {
+    render() {
+        return (
+           <PeepList />
+        )
+    }
+}
