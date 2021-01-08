7d39effdf9b578b652a907b2c269aea5cc68b373
charlie-galb <galbraith.charlie@gmail.com>
2021-01-08 16:53:12 +0000

    Refacting: change to functional component using hooks and context API to store user data

--- src/components/SignupForm/SignupForm.js
+++ src/components/SignupForm/SignupForm.js
@@ -1,58 +1,65 @@
-import React, { Component } from 'react';
+import React, { useContext, useState } from 'react';
 import { Form, Button } from 'react-bootstrap';
 import axios from 'axios';
+import { UserContext } from '../../contexts/UserContext.js';
 
-export default class SignupForm extends Component {
+const SignupForm = () => {
+ 
+    const { storeUserHandleInContext, storeUserPasswordInContext, storeUserIdInContext } = useContext(UserContext)
+    const [handle, setHandle] = useState("");
+    const [password, setPassword] = useState("");
+    const [passwordConfirmation, setPasswordConfirmation] = useState("");
 
-    constructor(props) {
-        super(props);
-        this.state = {
-          handle: "",
-          password: "",
-          passwordConfirmation: ""
-        };
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    };
-
-    handleChange(event) {
-        const { name, value } = event.target; 
-        this.setState({ [name]: value });
+    const handleHandleChange = (event) => {
+        const { value } = event.target; 
+        setHandle(value);
       }
 
-    async handleSubmit(event) {
+    const handlePasswordChange = (event) => {
+        const { value } = event.target; 
+        setPassword(value);
+      }
+
+    const handlePasswordConfirmationChange = (event) => {
+        const { value } = event.target; 
+        setPasswordConfirmation(value);
+      }
+
+    const handleSubmit = async (event) => {
         event.preventDefault();
-        const { handle, password, passwordConfirmation } = this.state;
         try {
             const response = await axios.post(
                 "https://chitter-backend-api-v2.herokuapp.com/users", {user: {handle: handle, password: password }}
             );
-            console.log(response)
+            if (response.data.handle === handle) {
+                storeUserHandleInContext(handle)
+                storeUserPasswordInContext(password)
+                storeUserIdInContext(response.data.id)
+            }
         } catch (error) {
             console.log("Error:", error)
         }
     }
+    return (
+        <Form>
+            <Form.Group controlId="sign-up-handle">
+                <Form.Label>Handle</Form.Label>
+                <Form.Control type="text" placeholder="Enter handle" data-testid="sign-up-handle-input" name="handle"value={handle} onChange={handleHandleChange} required/>
+            </Form.Group>
 
-    render() {
-        return (
-            <Form>
-                <Form.Group controlId="sign-up-handle">
-                    <Form.Label>Handle</Form.Label>
-                    <Form.Control type="text" placeholder="Enter handle" data-testid="sign-up-handle-input" name="handle"value={this.state.handle} onChange={this.handleChange} required/>
-                </Form.Group>
-
-                <Form.Group controlId="sign-up-password">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" data-testid="sign-up-password-input" name="password" value={this.state.password} onChange={this.handleChange} required/>
-                </Form.Group>
-                <Form.Group controlId="sign-up-password-confirmation">
-                    <Form.Label>Password Confirmation</Form.Label>
-                    <Form.Control type="password" placeholder="Confirm password" data-testid="sign-up-password-confirmation-input" name="passwordConfirmation" value={this.state.passwordConfirmation} onChange={this.handleChange} required/>
-                </Form.Group>
-                <Button variant="primary" type="submit" onClick={this.handleSubmit}>
-                    Submit
-                </Button>
-            </Form>
-        )
-    }
+            <Form.Group controlId="sign-up-password">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" data-testid="sign-up-password-input" name="password" value={password} onChange={handlePasswordChange} required/>
+            </Form.Group>
+            <Form.Group controlId="sign-up-password-confirmation">
+                <Form.Label>Password Confirmation</Form.Label>
+                <Form.Control type="password" placeholder="Confirm password" data-testid="sign-up-password-confirmation-input" name="passwordConfirmation" value={passwordConfirmation} onChange={handlePasswordConfirmationChange} required/>
+            </Form.Group>
+            <Button variant="primary" type="submit" onClick={handleSubmit}>
+                Submit
+            </Button>
+        </Form>
+    )
 }
+
+export default SignupForm;
