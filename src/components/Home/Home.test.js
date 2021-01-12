65b65aa51c383d2e3c2cbf5358f30b38429a5b01
charlie-galb <galbraith.charlie@gmail.com>
2021-01-12 14:01:24 +0000

    Add context wrapper to text component

--- src/components/Home/Home.test.js
+++ src/components/Home/Home.test.js
@@ -2,9 +2,10 @@ import React from 'react';
 import { render } from "@testing-library/react";
 import {BrowserRouter as Router} from 'react-router-dom';
 import Home from './Home.js';
+import UserContextProvider from '../../contexts/UserContext.js';
 
 test('renders without crashing', () => {
-    const app = render(<Router><Home /></Router>);
+    const app = render(<UserContextProvider><Router><Home /></Router></UserContextProvider>);
     expect(app.getByTestId("home-h1").textContent).toBe("Welcome to Chitter!");
     expect(app.getByTestId("home-h2").textContent).toBe("Please log in below");
 });
