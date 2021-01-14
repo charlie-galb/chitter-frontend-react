e1fb015baa6fe7a2485ac3ff284a6c0f400bc1c9
charlie-galb <galbraith.charlie@gmail.com>
2021-01-14 15:29:58 +0000

    Remove password storage in context state

--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -5,17 +5,12 @@ export const UserContext = createContext();
 class UserContextProvider extends Component {
     state = {
         userHandle: "",
-        userPassword: "",
         userId: "",
         currentSessionKey: ""
     };
 
     storeUserHandleInContext = (handle) => {
         this.setState({userHandle: handle})
-    };
-
-    storeUserPasswordInContext = (password) => {
-        this.setState({userPassword: password})
     };
 
     storeUserIdInContext = (id) => {
@@ -31,7 +26,6 @@ class UserContextProvider extends Component {
             <UserContext.Provider value={{ 
                 ...this.state,
                 storeUserHandleInContext: this.storeUserHandleInContext,
-                storeUserPasswordInContext: this.storeUserPasswordInContext,
                 storeUserIdInContext: this.storeUserIdInContext,
                 storeCurrentSessionKeyInContext: this.storeCurrentSessionKeyInContext,
                 }}>{this.props.children}</UserContext.Provider>
