b95c03512b9de965cdf41219fc14f147ac037d60
charlie-galb <galbraith.charlie@gmail.com>
2021-01-14 15:27:48 +0000

    Logout now wipes userContext state

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import {Link} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { UserContext } from '../../contexts/UserContext.js';
 
 function Navbar() {
+
+  const { storeUserHandleInContext, storeUserIdInContext, storeCurrentSessionKeyInContext } = useContext(UserContext)
+
+
+  const handleLogout = () => {
+    console.log("Click is working")
+    storeUserHandleInContext('') 
+    storeUserIdInContext('') 
+    storeCurrentSessionKeyInContext('')
+  }
+
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <a className="navbar-brand" href="#">Navbar</a>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
+        <a className="navbar-brand" href="#">Chitter</a>
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav">
-            <li className="nav-item">
+            <li className="nav-item" onClick={handleLogout}>
               <Link to='/' className="nav-link" id="log-out-link" data-testid="log-out-link" >Log out</Link>
             </li>
           </ul>
