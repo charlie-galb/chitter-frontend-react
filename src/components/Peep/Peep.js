7398e80c73baf7cb0db30d0a29e047fef2ce6e44
charlie-galb <galbraith.charlie@gmail.com>
2021-01-14 16:41:46 +0000

    Format using React Bootstrap Card

--- src/components/Peep/Peep.js
+++ src/components/Peep/Peep.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
+import { Card, Button } from 'react-bootstrap';
 
 class Peep extends Component {
     constructor(props){
         super(props)
-        console.log(this.props)
     }
 
     render() {
         return (
-            <p>{this.props.peepData.body}</p>
+            <div>
+                <Card border="dark" style={{ width: '18rem' }}>
+                    <Card.Header>{this.props.peepData.user.handle}</Card.Header>
+                    <Card.Subtitle className="mb-2 text-muted">Posted at {this.props.peepData.created_at}</Card.Subtitle>
+                    <Card.Body>
+                        <Card.Text>{this.props.peepData.body}</Card.Text>
+                        <Button variant="secondary" size="sm">Like</Button>
+                        <Card.Footer className="text-muted">Liked by {this.props.peepData.likes.length}</Card.Footer>
+                    </Card.Body>
+                </Card>
+                <br />
+            </div>
         )
     }
 
