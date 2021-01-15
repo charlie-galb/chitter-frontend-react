513facf2a48b57c4c9969fa7975f8de882e40d80
charlie-galb <galbraith.charlie@gmail.com>
2021-01-15 15:37:31 +0000

    Add test for delete button

--- src/components/Peep/Peep.test.js
+++ src/components/Peep/Peep.test.js
@@ -1,30 +1,45 @@
 import React from 'react';
-import { render, screen } from "@testing-library/react";
+import {render, fireEvent} from '@testing-library/react';
+import axios from 'axios';
+import { UserContext } from '../../contexts/UserContext.js';
 import Peep from './Peep.js';
 
- describe("PeepList", () => {
+ describe("Peep", () => {
 
-    const mockPeepData = {
-        "id": 3,
-        "body": "my first peep :)",
-        "created_at": "2018-06-23T13:21:23.317Z",
-        "updated_at": "2018-06-23T13:21:23.317Z",
+  const mockContext = {
+    userId: "1",
+    currentSessionKey: "_2a_12_A9rsHFpqa9xB0k_lIdNlH_"
+  }
+
+  const mockPeepData = {
+      "id": 3,
+      "body": "my first peep :)",
+      "created_at": "2018-06-23T13:21:23.317Z",
+      "updated_at": "2018-06-23T13:21:23.317Z",
+      "user": {
+        "id": 1,
+        "handle": "kay"
+      },
+      "likes": [{
         "user": {
           "id": 1,
           "handle": "kay"
-        },
-        "likes": [{
-          "user": {
-            "id": 1,
-            "handle": "kay"
-          }
-        }]
-    }
+        }
+      }]
+  }
+
+  const retrievePeeps = jest.fn()
   
   test('renders without crashing', () => {
-    const peep = render(<Peep key='1' peepData={mockPeepData}/>);
-    screen.debug()
+    const peep = render(<UserContext.Provider value={mockContext}><Peep key='1' peepData={mockPeepData}/></UserContext.Provider>);
     expect(peep).toMatchSnapshot()
+  });
+
+  test('can delete itself', () => {
+    let axiosSpy = jest.spyOn(axios, "delete")
+    const { getByText } = render(<UserContext.Provider value={mockContext}><Peep retrievePeeps={retrievePeeps} key='1' peepData={mockPeepData}/></UserContext.Provider>);
+    fireEvent.click(getByText('Delete'))
+    expect(axiosSpy).toHaveBeenCalled()
   });
 
  }) 
