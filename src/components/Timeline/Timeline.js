bfaf6e2a91b30ebff6162c4b6df1fd7a3605c6ef
charlie-galb <galbraith.charlie@gmail.com>
2021-01-15 13:29:47 +0000

    Conver to functional component and add API request (moved from PeepList)

--- src/components/Timeline/Timeline.js
+++ src/components/Timeline/Timeline.js
@@ -1,15 +1,29 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios';
 import PeepList from '../PeepList/PeepList.js';
 import NewPeepForm from '../NewPeepForm/NewPeepForm.js'
 
-export default class Timeline extends Component {
-    render() {
-        return (
-            <div>
-                <h2 data-testid="timeline-h2">Timeline</h2>
-                <NewPeepForm />
-                <PeepList />
-            </div>
-        )
-    }
+const Timeline = () => {
+
+    const [peeps, setPeeps] = useState(null)
+
+    const retrievePeeps = async () => {
+        await axios.get("https://chitter-backend-api-v2.herokuapp.com/peeps")
+        .then((response) => {setPeeps(response.data)})
+      }
+      
+      useEffect( () => {
+        retrievePeeps()
+      }, []);
+ 
+    return (
+        <div>
+            <h2 data-testid="timeline-h2">Timeline</h2>
+            <NewPeepForm retrievePeeps={retrievePeeps}/>
+            <PeepList peeps={peeps} />
+        </div>
+    )
+    
 }
+
+export default Timeline;
