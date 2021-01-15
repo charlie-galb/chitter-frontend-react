a85b11e908d04d11a493ba0228e8d1330e13b6f5
charlie-galb <galbraith.charlie@gmail.com>
2021-01-15 13:44:28 +0000

    Remove redundant test and add mockPeepData as prop to existing test

--- src/components/PeepList/PeepList.test.js
+++ src/components/PeepList/PeepList.test.js
@@ -5,25 +5,30 @@ import PeepList from './PeepList.js';
 
  describe("PeepList", () => {
 
-  beforeAll(() => {
-    jest.mock("axios")
-  })
-
-  afterAll(() => {
-    jest.resetAllMocks()
-  })
+  const mockPeepData = [
+    {
+      "id": 3,
+      "body": "my first peep :)",
+      "created_at": "2018-06-23T13:21:23.317Z",
+      "updated_at": "2018-06-23T13:21:23.317Z",
+      "user": {
+        "id": 1,
+        "handle": "kay"
+      },
+      "likes": [{
+        "user": {
+          "id": 1,
+          "handle": "kay"
+        }
+      }]
+    }
+  ]
+  
 
   test('renders without crashing', () => {
     const peepListContainer = document.createElement("div");
-    ReactDOM.render(<PeepList />, peepListContainer);
-    expect(peepListContainer.querySelector("p").textContent).toBe("OneTwoThree");
-  });
-
-  test('makes a get request', () => {
-    let axiosSpy = jest.spyOn(axios, "get")
-    const peepListContainer = document.createElement("div");
-    ReactDOM.render(<PeepList />, peepListContainer)
-    expect(axiosSpy).toHaveBeenCalledWith("https://chitter-backend-api-v2.herokuapp.com/peeps")
+    ReactDOM.render(<PeepList peeps={mockPeepData}/>, peepListContainer);
+    expect(peepListContainer.querySelector("p").textContent).toBe("my first peep :)");
   });
 
  }) 
