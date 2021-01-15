6c074d69798b54c576e0e74358d4da57c5d307a4
charlie-galb <galbraith.charlie@gmail.com>
2021-01-15 15:34:00 +0000

    Convert to functional component and add delete functionality

--- src/components/Peep/Peep.js
+++ src/components/Peep/Peep.js
@@ -1,28 +1,46 @@
-import React, { Component } from 'react';
+import React, { useContext } from 'react';
 import { Card, Button } from 'react-bootstrap';
+import axios from 'axios';
+import { UserContext } from '../../contexts/UserContext.js';
 
-class Peep extends Component {
-    constructor(props){
-        super(props)
+const Peep = (props) => {
+
+    const { userId, currentSessionKey } = useContext(UserContext)
+
+    const handleDelete = async (event) => {
+        event.preventDefault();
+        try {
+            await axios.delete(`https://chitter-backend-api-v2.herokuapp.com/peeps/${props.peepData.id}`, 
+            {headers: {
+                  Authorization: `Token ${currentSessionKey}` 
+                }});
+            props.retrievePeeps()
+        } catch (error) {
+            console.log("Error:", error)
+        }
     }
 
-    render() {
-        return (
-            <div>
-                <Card border="dark" style={{ width: '18rem' }}>
-                    <Card.Header>{this.props.peepData.user.handle}</Card.Header>
-                    <Card.Subtitle className="mb-2 text-muted">Posted at {this.props.peepData.created_at}</Card.Subtitle>
-                    <Card.Body>
-                        <Card.Text>{this.props.peepData.body}</Card.Text>
-                        <Button variant="secondary" size="sm">Like</Button>
-                        <Card.Footer className="text-muted">Liked by {this.props.peepData.likes.length}</Card.Footer>
-                    </Card.Body>
-                </Card>
-                <br />
-            </div>
-        )
+    const renderDeleteButton = () => {
+        if (userId == props.peepData.user.id) {
+            return <Button variant="secondary" size="sm" onClick={handleDelete}>Delete</Button>
+        }
     }
 
-} 
+    return (
+        <div>
+            <Card border="dark" style={{ width: '18rem' }}>
+                <Card.Header>{props.peepData.user.handle}</Card.Header>
+                <Card.Subtitle className="mb-2 text-muted">Posted at {props.peepData.created_at}</Card.Subtitle>
+                <Card.Body>
+                    <Card.Text>{props.peepData.body}</Card.Text>
+                    <Button variant="secondary" size="sm">Like</Button>
+                    {renderDeleteButton()}
+                    <Card.Footer className="text-muted">Liked by {props.peepData.likes.length}</Card.Footer>
+                </Card.Body>
+            </Card>
+            <br />
+        </div>
+    )
+}
 
 export default Peep;
