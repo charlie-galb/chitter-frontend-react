0247485dacec0123858a82cfb5772dc2d5a95d72
charlie-galb <galbraith.charlie@gmail.com>
2021-01-15 13:08:57 +0000

    Add form for making new posts and handleSubmit test

---
+++ src/components/NewPeepForm/NewPeepForm.js
@@ -0,0 +1,48 @@
+import React, { useContext, useState } from 'react';
+import { Form, Button } from 'react-bootstrap';
+import { Redirect } from 'react-router-dom';
+import axios from 'axios';
+import { UserContext } from '../../contexts/UserContext.js';
+
+const NewPeepForm = () => {
+ 
+    const { userHandle, userId, currentSessionKey } = useContext(UserContext)
+    const [peepBody, setPeepBody] = useState("")
+
+    const handlePeepBodyChange = (event) => {
+        const { value } = event.target; 
+        setPeepBody(value);
+      }
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            console.log(peepBody)
+            console.log(userId)
+            console.log(currentSessionKey)
+            const response = await axios.post("https://chitter-backend-api-v2.herokuapp.com/peeps", {peep: {user_id:`${userId}`, body:`${peepBody}`}}, 
+            {headers: {
+                  Authorization: `Token ${currentSessionKey}` 
+                }});
+            if (response.data) {
+                console.log(response.data)
+            }
+        } catch (error) {
+            console.log("Error:", error)
+        }
+    }
+
+    return (
+        <Form>
+            <Form.Group controlId="new-peep">
+                <Form.Label>Got something to say?</Form.Label>
+                <Form.Control type="textarea" placeholder="Say it here!" data-testid="peep-body-text-input" name="new-peep-body" value={peepBody} onChange={handlePeepBodyChange} required/>
+            </Form.Group>
+            <Button variant="primary" type="submit" onClick={handleSubmit}>
+                Post peep
+            </Button>
+        </Form>
+    )
+}
+
+export default NewPeepForm;
---
+++ src/components/NewPeepForm/NewPeepForm.test.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {render, fireEvent, cleanup} from '@testing-library/react';
+import axios from "axios";
+import NewPeepForm from './NewPeepForm.js';
+import { UserContext } from '../../contexts/UserContext.js';
+
+  afterAll(() => {
+    jest.resetAllMocks()
+  })
+
+afterEach(cleanup)
+
+ it('submits the correct info to the api', () => {
+    const mockContext = {
+      userHandle: "Test Person",
+      userId: "1",
+      currentSessionKey: "_2a_12_A9rsHFpqa9xB0k_lIdNlH_"
+    }
+    let axiosSpy = jest.spyOn(axios, "post")
+    const { getByText, getByTestId } = render(<UserContext.Provider value={mockContext}><NewPeepForm /></UserContext.Provider>);
+    fireEvent.change(getByTestId("peep-body-text-input"), {target: {value: 'This peep is for test purposes' } } )
+    fireEvent.click(getByText('Post peep'))
+    setTimeout(() => {
+        expect(axiosSpy).not.toHaveBeenCalledWith("https://chitter-backend-api-v2.herokuapp.com/peeps", {"peep": {"user_id":"1", "body":"This peep is for test purposes"}}, 
+        {"headers": {
+              'Authorization': `${mockContext.currentSessionKey}` 
+            }});
+      }, 0)
+ })
+
