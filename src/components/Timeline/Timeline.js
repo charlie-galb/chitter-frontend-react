7b7e65a78919b5ae68f7fd40f38ba36f2ba65671
charlie-galb <galbraith.charlie@gmail.com>
2021-01-15 13:11:27 +0000

    Add NewPeepForm component

--- src/components/Timeline/Timeline.js
+++ src/components/Timeline/Timeline.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import PeepList from '../PeepList/PeepList.js';
+import NewPeepForm from '../NewPeepForm/NewPeepForm.js'
 
 export default class Timeline extends Component {
     render() {
         return (
             <div>
                 <h2 data-testid="timeline-h2">Timeline</h2>
+                <NewPeepForm />
                 <PeepList />
             </div>
         )
