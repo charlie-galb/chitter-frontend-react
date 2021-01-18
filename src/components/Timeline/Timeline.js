dcef9a033cf4b9e36c3b56b268ab77f3d92a3d48
charlie-galb <galbraith.charlie@gmail.com>
2021-01-18 17:10:57 +0000

    Add conditional redirect for if the uer is not signed in

--- src/components/Timeline/Timeline.js
+++ src/components/Timeline/Timeline.js
@@ -1,21 +1,33 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useContext } from 'react'
+import { Redirect } from 'react-router-dom';
 import axios from 'axios';
 import PeepList from '../PeepList/PeepList.js';
 import NewPeepForm from '../NewPeepForm/NewPeepForm.js'
+import { UserContext } from '../../contexts/UserContext.js';
 
 const Timeline = () => {
 
+    const { currentSessionKey } = useContext(UserContext)
     const [peeps, setPeeps] = useState(null)
+    const [redirect, setRedirect] = useState(null)
 
     const retrievePeeps = async () => {
-        await axios.get("https://chitter-backend-api-v2.herokuapp.com/peeps")
-        .then((response) => {setPeeps(response.data)})
-      }
+      if (currentSessionKey === "") {setRedirect('/')}
+      try {
+        const response = await axios.get("https://chitter-backend-api-v2.herokuapp.com/peeps")
+        if (response.data) { setPeeps(response.data) } 
+      } catch (error) {
+            console.log("Error:", error)
+        }
+    }
       
       useEffect( () => {
         retrievePeeps()
       }, []);
  
+    if (redirect) {
+      return <Redirect to={redirect} data-testif='timeline-redirect-to-home'/>
+    }
     return (
         <div>
             <h2 data-testid="timeline-h2">Timeline</h2>
