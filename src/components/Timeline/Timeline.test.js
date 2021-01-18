d682a6b38a20e0c3ea3b087f196dc31f5fca1405
charlie-galb <galbraith.charlie@gmail.com>
2021-01-18 17:11:13 +0000

    Add test for conditional redirect to home

--- src/components/Timeline/Timeline.test.js
+++ src/components/Timeline/Timeline.test.js
@@ -1,10 +1,45 @@
 import React from 'react';
 import { render } from "@testing-library/react";
-import {BrowserRouter as Router} from 'react-router-dom';
+import {BrowserRouter as Router, Redirect} from 'react-router-dom';
 import Timeline from './Timeline.js';
-import UserContextProvider from '../../contexts/UserContext.js';
+import {UserContext} from '../../contexts/UserContext.js';
 
-test('renders without crashing', () => {
-    const app = render(<UserContextProvider><Router><Timeline /></Router></UserContextProvider>);
-    expect(app.getByTestId("timeline-h2").textContent).toBe("Timeline");
-});
+
+
+describe("Timeline", () => {
+
+    const mockPeepData = {
+        "id": 3,
+        "body": "my first peep :)",
+        "created_at": "2018-06-23T13:21:23.317Z",
+        "updated_at": "2018-06-23T13:21:23.317Z",
+        "user": {
+          "id": 1,
+          "handle": "kay"
+        },
+        "likes": [{
+          "user": {
+            "id": 1,
+            "handle": "kay"
+          }
+        }]
+    }
+  
+    const mockContext = {
+      userId: "1",
+      currentSessionKey: "_2a_12_A9rsHFpqa9xB0k_lIdNlH_",
+      userHandle: "Test Person"
+    }
+
+    test('renders without crashing', () => {
+        const timeline = render(<UserContext.Provider value={mockContext}><Router><Timeline /></Router></UserContext.Provider>);
+        expect(timeline.getByTestId("timeline-h2").textContent).toBe("Timeline");
+        expect(timeline.queryByTestId('timeline-redirect-to-home')).toBeNull()
+    });
+    
+    test('redirects to home if no current session key', () => {
+        mockContext.currentSessionKey = ""
+        const timeline = render(<UserContext.Provider value={mockContext}><Router><Timeline /></Router></UserContext.Provider>);
+        setTimeout(() => { expect(timeline.queryByTestId('timeline-redirect-to-home').toBe(<Redirect to="/" />)); }, 0)
+    });
+})
