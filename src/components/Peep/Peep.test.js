562066de41e908a80992a038b220811ae3c6afe4
charlie-galb <galbraith.charlie@gmail.com>
2021-03-03 12:35:16 +0000

    Change date in mockPeepData

--- src/components/Peep/Peep.test.js
+++ src/components/Peep/Peep.test.js
@@ -14,8 +14,8 @@ import Peep from './Peep.js';
   const mockPeepData = {
       "id": 3,
       "body": "my first peep :)",
-      "created_at": "2018-06-23T13:21:23.317Z",
-      "updated_at": "2018-06-23T13:21:23.317Z",
+      "created_at": "2018-01-23T13:21:23.317Z",
+      "updated_at": "2018-01-23T13:21:23.317Z",
       "user": {
         "id": 1,
         "handle": "kay"
--- src/components/Timeline/Timeline.test.js
+++ src/components/Timeline/Timeline.test.js
@@ -10,8 +10,8 @@ describe("Timeline", () => {
     const mockPeepData = {
         "id": 3,
         "body": "my first peep :)",
-        "created_at": "2018-06-23T13:21:23.317Z",
-        "updated_at": "2018-06-23T13:21:23.317Z",
+        "created_at": "2018-01-23T13:21:23.317Z",
+        "updated_at": "2018-01-23T13:21:23.317Z",
         "user": {
           "id": 1,
           "handle": "kay"
