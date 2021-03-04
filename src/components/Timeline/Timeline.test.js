cf2e0a5660362b2af59a060043fafc26003925b0
charlie-galb <galbraith.charlie@gmail.com>
2021-03-04 12:56:39 +0000

    Debugging remove setTimeout and redo redirect test

--- src/components/Timeline/Timeline.test.js
+++ src/components/Timeline/Timeline.test.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import axios from "axios";
 import { render } from "@testing-library/react";
-import {BrowserRouter as Router, Redirect} from 'react-router-dom';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+
 import Timeline from './Timeline.js';
-import {UserContext} from '../../contexts/UserContext.js';
+import { UserContext } from '../../contexts/UserContext.js';
 
 describe("Timeline", () => {
 
-    const mockPeepData = {
-        "id": 3,
+    const mockResponse = { data: [{"id": 3,
         "body": "my first peep :)",
         "created_at": "2018-01-23T13:21:23.317Z",
         "updated_at": "2018-01-23T13:21:23.317Z",
@@ -20,6 +20,7 @@ describe("Timeline", () => {
           "id": 1,
           "user_id": 2
         }]
+    }]
     }
   
     const mockContext = {
@@ -28,18 +29,25 @@ describe("Timeline", () => {
       userHandle: "Test Person"
     }
 
-    const axiosSpy = jest.spyOn(axios, "get")
+    axios.get = jest.fn()
 
     test('renders without crashing', () => {
         const timeline = render(<UserContext.Provider value={mockContext}><Router><Timeline /></Router></UserContext.Provider>);
         expect(timeline.getByTestId("timeline-h2").textContent).toBe("Timeline");
         expect(timeline.queryByTestId('timeline-redirect-to-home')).toBeNull()
-        expect(axiosSpy).toHaveBeenCalledWith(`${process.env.BACKEND_URL}/peeps`, {"headers": {"authorization": mockContext.currentSessionKey}})
+        expect(axios.get).toHaveBeenCalledWith(`${process.env.BACKEND_URL}/peeps`, {"headers": {"authorization": mockContext.currentSessionKey}})
     });
     
     test('redirects to home if no current session key', () => {
         mockContext.currentSessionKey = ""
-        const timeline = render(<UserContext.Provider value={mockContext}><Router><Timeline /></Router></UserContext.Provider>);
-        setTimeout(() => { expect(timeline.queryByTestId('timeline-redirect-to-home').toBe(<Redirect to="/" />)); }, 0)
+        const { 
+          container, 
+        } = render(<UserContext.Provider value={mockContext}>
+            <Router>
+              <Timeline />
+              <Route path='/'>home</Route>
+            </Router>
+          </UserContext.Provider>);
+        expect(container).toHaveTextContent(/home/)
     });
 })
