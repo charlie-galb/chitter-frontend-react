a11c54498099f6a07127989fa5d7779b90e6cb7a
charlie-galb <galbraith.charlie@gmail.com>
2021-03-12 19:11:52 +0000

    Add tests for conditional flash message display

--- src/components/LoginForm/LoginForm.test.js
+++ src/components/LoginForm/LoginForm.test.js
@@ -8,15 +8,14 @@ afterAll(() => {
   jest.resetAllMocks()
 })
 
-afterEach(cleanup)
+const mockContext = {
+  storeUserHandleInContext: jest.fn(),
+  storeUserIdInContext: jest.fn(),
+  storeCurrentSessionKeyInContext: jest.fn()
+}
 
 describe('LoginForm', () => {
   it('submits the correct info to the api', () => {
-    const mockContext = {
-      storeUserHandleInContext: jest.fn(),
-      storeUserIdInContext: jest.fn(),
-      storeCurrentSessionKeyInContext: jest.fn()
-    }
     const axiosSpy = jest.spyOn(axios, "post")
     const { getByText, getByTestId } = render(<UserContext.Provider value={mockContext}><LoginForm /></UserContext.Provider>);
     fireEvent.change(getByTestId("log-in-handle-input"), {target: {value: 'test handle' } } )
@@ -24,5 +23,16 @@ describe('LoginForm', () => {
     fireEvent.click(getByText('Submit'))
     expect(axiosSpy).toHaveBeenCalledWith(`${process.env.REACT_APP_BACKEND_URL}/sessions`, {"session": {"handle":"test handle", "password":"test password"}})
  })
+ it('submits the correct info to the api', () => {
+  axios.post = jest.fn().mockImplementation(() => {
+    throw new Error('mock error');
+  })
+  const { container, getByText, getByTestId } = render(<UserContext.Provider value={mockContext}><LoginForm /></UserContext.Provider>);
+  fireEvent.change(getByTestId("log-in-handle-input"), {target: {value: 'test handle' } } )
+  fireEvent.change(getByTestId("log-in-password-input"), {target: {value: 'test password' } } )
+  fireEvent.click(getByText('Submit'))
+  expect(axios.post).toThrow("mock error")
+  expect(container.textContent).toMatch(/Invalid handle or password/)
+})
 })
  
