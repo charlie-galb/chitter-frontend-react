88a373c7418c6710fa8fcb077b8040cede17ef2a
charlie-galb <galbraith.charlie@gmail.com>
2021-03-16 12:17:38 +0000

    Update test name with new component name

--- src/components/FlashMessage/FlashMessage.test.js
+++ src/components/FlashMessage/FlashMessage.test.js
@@ -3,7 +3,7 @@ import { render, act } from '@testing-library/react';
 
 import FlashMessage from './FlashMessage.js';
 
-describe('Notice', () => {
+describe('FlashMessage', () => {
     it('displays when displayMessage === true, then disappears after five seconds', () => {
         jest.useFakeTimers()
         const {container} = render(
