bc9ac08652d6e86bae6eb591b8cc1c6cbf522a18
charlie-galb <galbraith.charlie@gmail.com>
2021-03-16 11:45:56 +0000

    Debug redirect test and clean up formatting

--- src/components/Timeline/Timeline.test.js
+++ src/components/Timeline/Timeline.test.js
@@ -1,53 +1,36 @@
 import React from 'react';
 import axios from "axios";
-import { render } from "@testing-library/react";
-import {BrowserRouter as Router, Route} from 'react-router-dom';
 
+import renderWithRouter from '../../utils/testUtils/renderWithRouter'
 import Timeline from './Timeline.js';
 import { UserContext } from '../../contexts/UserContext.js';
 
+const mockContext = {
+  userId: "1",
+  currentSessionKey: "_2a_12_A9rsHFpqa9xB0k_lIdNlH_",
+  userHandle: "Test Person"
+}
+
+axios.get = jest.fn()
+
+const ui = <UserContext.Provider value={mockContext}>
+            <Timeline />
+          </UserContext.Provider>
+
 describe("Timeline", () => {
-
-    const mockResponse = { data: [{"id": 3,
-        "body": "my first peep :)",
-        "created_at": "2018-01-23T13:21:23.317Z",
-        "updated_at": "2018-01-23T13:21:23.317Z",
-        "user": {
-          "id": 1,
-          "handle": "kay"
-        },
-        "likes": [{
-          "id": 1,
-          "user_id": 2
-        }]
-    }]
-    }
-  
-    const mockContext = {
-      userId: "1",
-      currentSessionKey: "_2a_12_A9rsHFpqa9xB0k_lIdNlH_",
-      userHandle: "Test Person"
-    }
-
-    axios.get = jest.fn()
-
     test('renders without crashing', () => {
-        const timeline = render(<UserContext.Provider value={mockContext}><Router><Timeline /></Router></UserContext.Provider>);
-        expect(timeline.getByTestId("timeline-h2").textContent).toBe("Timeline");
-        expect(timeline.queryByTestId('timeline-redirect-to-home')).toBeNull()
-        expect(axios.get).toHaveBeenCalledWith(`${process.env.REACT_APP_BACKEND_URL}/peeps`, {"headers": {"authorization": mockContext.currentSessionKey}})
+      const { history, getByTestId, queryByTestId } = renderWithRouter(ui, '/timeline')
+      expect(getByTestId("timeline-h2").textContent).toBe("Timeline");
+      expect(queryByTestId('timeline-redirect-to-home')).toBeNull()
+      expect(axios.get).toHaveBeenCalledWith(
+        `${process.env.REACT_APP_BACKEND_URL}/peeps`, 
+        {"headers": {"authorization": mockContext.currentSessionKey}}
+        );
+      expect(history.location.pathname).toEqual('/timeline')
     });
-    
     test('redirects to home if no current session key', () => {
-        mockContext.currentSessionKey = ""
-        const { 
-          container, 
-        } = render(<UserContext.Provider value={mockContext}>
-            <Router>
-              <Timeline />
-              <Route path='/'>home</Route>
-            </Router>
-          </UserContext.Provider>);
-        expect(container).toHaveTextContent(/home/)
+      mockContext.currentSessionKey = ""
+      const { history } = renderWithRouter(ui, '/timeline')
+      expect(history.location.pathname).toEqual('/')
     });
 })
