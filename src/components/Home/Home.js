ecabb1b7e2ca5760db93a0a69a25f1b96e88a2a1
charlie-galb <galbraith.charlie@gmail.com>
2021-03-29 16:37:18 +0100

    Add logo and custom css classes

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,16 +1,23 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
+
 import LoginForm from '../LoginForm/LoginForm.js';
+import styles from './Home.module.css'
 
 export default class Home extends Component {
     render() {
         return (
-            <div>
-                <h1 data-testid="home-h1">Welcome to Chitter!</h1>
-                <h2 data-testid="home-h2">Please log in below</h2>
-                <LoginForm />
-                <p>Don't have an account?</p>
-                <Link data-testid="sign-up-link" to='/sign_up'>Sign up here</Link>
+            <div className={styles.container}>
+                <div className={styles.logoContainer}>
+                    <img src='/chitter-logo-square.png' alt='chitter-logo' className={styles.logo} width="80%" />
+                </div>
+                <div className={styles.formContainer}>
+                    <h1 data-testid="home-h1">Welcome to Chitter!</h1>
+                    <h2 data-testid="home-h2">Please log in below</h2>
+                    <LoginForm />
+                    <p>Don't have an account?</p>
+                    <Link data-testid="sign-up-link" to='/sign_up'>Sign up here</Link>
+                </div>
             </div>
         )
     }
