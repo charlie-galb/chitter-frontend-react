9e2b34999dda7baac3b901536875a540a0a25a64
charlie-galb <galbraith.charlie@gmail.com>
2021-03-29 16:37:42 +0100

    Replace bootstrap with custom styling

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,8 +1,8 @@
 import React, { useContext } from 'react';
 import {Link} from 'react-router-dom';
-import { Navbar as BootNavbar } from 'react-bootstrap';
-import Nav from 'react-bootstrap/Nav'
+
 import { UserContext } from '../../contexts/UserContext.js';
+import styles from './Navbar.module.css';
 
 function Navbar() {
 
@@ -17,17 +17,15 @@ function Navbar() {
 
   const renderLogoutLink = () => {
     if (currentSessionKey !== "") {
-      return <Link to='/' className="nav-link" id="log-out-link" data-testid="log-out-link" onClick={handleLogout}>Log out</Link>
+      return <Link to='/' className="nav-link" id="logOutLink" data-testid="log-out-link" onClick={handleLogout} >Log out</Link>
     }
   }
 
     return (
-      <BootNavbar bg="light" variant="light">
-        <Link to='/' className="navbar-brand" data-testid='navbar-logo-link'>Chitter</Link>
-        <Nav className="ml-auto">
-          {renderLogoutLink()}
-        </Nav>
-      </BootNavbar>
+     <div style={{width: '100%', height: "70px", backgroundColor: '#10D48E', display: 'fixed'}}>
+       <img src='/chitter-logo-long.png' alt='chitter-logo' height="100%" className={styles.logo}/>
+       {renderLogoutLink()}
+     </div>
     )
 }
 
