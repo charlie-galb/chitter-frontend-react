af4bd5fc1217566f710e79f7767d4734f437f989
charlie-galb <galbraith.charlie@gmail.com>
2021-03-30 11:07:46 +0100

    Convert to functional component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,24 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {Link} from 'react-router-dom';
 
 import LoginForm from '../LoginForm/LoginForm.js';
 import styles from './Home.module.css'
 
-export default class Home extends Component {
-    render() {
-        return (
-            <div className={styles.container}>
-                <div className={styles.logoContainer}>
-                    <img src='/chitter-logo-square.png' alt='chitter-logo' className={styles.logo} width="80%" />
-                </div>
-                <div className={styles.formContainer}>
-                    <h1 data-testid="home-h1">Welcome to Chitter!</h1>
-                    <h2 data-testid="home-h2">Please log in below</h2>
-                    <LoginForm />
-                    <p>Don't have an account?</p>
-                    <Link data-testid="sign-up-link" to='/sign_up'>Sign up here</Link>
-                </div>
+const Home = () => {
+    
+    return (
+        <div className={styles.container}>
+            <div className={styles.logoContainer}>
+                <img src='/chitter-logo-square.png' alt='chitter-logo' className={styles.logo} width="80%" />
             </div>
-        )
-    }
+            <div className={styles.formContainer}>
+                <h1 data-testid="home-h1">Welcome to Chitter!</h1>
+                <h2 data-testid="home-h2">Please log in below</h2>
+                <LoginForm />
+                <p>Don't have an account?</p>
+                <Link data-testid="sign-up-link" to='/sign_up'>Sign up here</Link>
+            </div>
+        </div>
+    )
+   
 }
+
+export default Home
