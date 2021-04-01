ebd41de0cfca127fe09c105e4cad91f2a89d3816
charlie-galb <galbraith.charlie@gmail.com>
2021-04-01 11:44:35 +0100

    Remove form labels

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -47,15 +47,12 @@ const LoginForm = () => {
         <div>
             <Form>
                 <Form.Group controlId="log-in-handle">
-                    <Form.Label>Handle</Form.Label>
-                    <Form.Control type="text" placeholder="Enter handle" data-testid="log-in-handle-input" name="handle"value={handle} onChange={handleHandleChange} required/>
+                    <Form.Control type="text" placeholder="Enter handle" className="textInput" data-testid="log-in-handle-input" name="handle"value={handle} onChange={handleHandleChange} required/>
                 </Form.Group>
-
                 <Form.Group controlId="log-in-password">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" data-testid="log-in-password-input" name="password" value={password} onChange={handlePasswordChange} required/>
+                    <Form.Control type="password" placeholder="Password" className="textInput" data-testid="log-in-password-input" name="password" value={password} onChange={handlePasswordChange} required/>
                 </Form.Group>
-                <Button variant="primary" type="submit" onClick={handleSubmit}>
+                <Button variant="primary" className="formButton" type="submit" onClick={handleSubmit}>
                     Submit
                 </Button>
             </Form>
