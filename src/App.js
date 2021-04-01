02b56d0cf98d50d4bb3f2ac8261c406b62155427
charlie-galb <galbraith.charlie@gmail.com>
2021-04-01 11:43:58 +0100

    Add SignupPage component (now parent of SignupForm)

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import UserContextProvider from './contexts/UserContext.js';
 import Timeline from './components/Timeline/Timeline.js';
 import Home from './components/Home/Home.js';
-import SignupForm from './components/SignupForm/SignupForm.js';
+import SignupPage from './components/SignupPage/SignupPage.js';
 
 function App() {
   return (
@@ -14,7 +14,7 @@ function App() {
           <Switch>
             <Route exact path='/' component={Home} />
             <Route path='/timeline' component={Timeline} />
-            <Route path='/sign_up' component={SignupForm} />
+            <Route path='/sign_up' component={SignupPage} />
           </Switch>
         </div>
       </UserContextProvider>
