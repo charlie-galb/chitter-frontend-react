e36ee38c104340f9c86ea2290a1969ce817d53a1
charlie-galb <galbraith.charlie@gmail.com>
2021-04-06 12:29:10 +0100

    Remove unused imports

--- src/components/SignupForm/SignupForm.test.js
+++ src/components/SignupForm/SignupForm.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {render, fireEvent, act} from '@testing-library/react';
+import {fireEvent, act} from '@testing-library/react';
 import axios from "axios";
 
 import renderWithRouter from '../../utils/testUtils/renderWithRouter'
