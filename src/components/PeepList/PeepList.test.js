83f15f1ab4c36798632fa6d7a2242b8e28b1026a
charlie-galb <galbraith.charlie@gmail.com>
2021-04-06 12:29:18 +0100

    Remove unused const

--- src/components/PeepList/PeepList.test.js
+++ src/components/PeepList/PeepList.test.js
@@ -6,21 +6,6 @@ import PeepList from './PeepList.js';
 
  describe("Peeplist", () => {
 
-  const mockPeepData = {
-      "id": 3,
-      "body": "my first peep :)",
-      "created_at": "2018-06-23T13:21:23.317Z",
-      "updated_at": "2018-06-23T13:21:23.317Z",
-      "user": {
-        "id": 1,
-        "handle": "kay"
-      },
-      "likes": [{
-        "id": 1,
-        "user_id": 2
-      }]
-  }
-
   const mockContext = {
     userId: "1",
     currentSessionKey: "_2a_12_A9rsHFpqa9xB0k_lIdNlH_"
